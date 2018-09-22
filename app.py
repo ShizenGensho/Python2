d7c38a183b9da38b620e015b6abe0f243bf939c8
Dustmaner <erick.soy@hotmail.com>
2018-09-22 07:40:49 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+character_name = "Murasame"
+character_age = "8"
+
+print("There once was a boat named "+ character_name + ", ")
+print("She was "+ character_age + " years old.")
+print(character_name + " really liked helping her friends,")
+print("But " + character_name + " only got to be " + character_age + " years old.")
